ea169c65a02ff0bf4905f109cdc290328fbf714a
Mjhdaais <mjhdaais@gmail.com>
2021-06-25 14:15:00 +0100

    made some clean up in routes.js

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,38 +1,9 @@
 import { Route, Switch } from 'react-router-dom'
 import Dashboard from  '../components/Dashboard/Dashboard'
-import Wallet from '../components/Wallet/Wallet'
 import NotFound from '../components/NotFound/NotFound'
 import SignIn from '../components/SignIn/SignIn'
 import SignUp from '../components/SignUp/SignUp'
 import Landing from '../components/Landing/Landing'
-
-/*import { Auth, API, graphqlOperation } from 'aws-amplify'
-import { createClient } from '../graphql/mutations'
-import { onError } from '../components/libs/errorLib'*/
-
-const Home = () => {
-    {/*async function saveClient() {
-        try {
-            const client = {
-                referalCode: 'clxr3452hs', 
-                status: 'CONTRIBUTOR', 
-                rule: 'RED'
-            }
-        
-            await API.graphql(graphqlOperation(createClient, { input: client }))
-    
-        } catch (e) {
-            onError(e)
-        }
-    }*/}
-
-    return (
-        <>
-            {/*<button onClick={saveClient}>Save Client</button>*/}
-            <h1>Home Page</h1>
-        </>
-    )
-}
 
 export default function Routes(){
     return (
@@ -40,17 +11,14 @@ export default function Routes(){
             <Route exact path='/'>
                 <Landing />
             </Route>
-            <Route exact path='/dashboard'>
-                <Dashboard />
-            </Route>
             <Route exact path='/signin'>
                 <SignIn />
             </Route>
             <Route exact path="/signup">
                 <SignUp />
             </Route>
-            <Route exact path='/wallet'>
-                <Wallet />
+            <Route exact path='/dashboard'>
+                <Dashboard />
             </Route>
             <Route>
                 <NotFound />
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,156 +1,67 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import 'antd/dist/antd.css'
+import './Dashboard.css'
+import { Layout, Menu, Typography, Row, Col, Button } from 'antd'
+import { DashboardOutlined, HistoryOutlined, SettingOutlined, LogoutOutlined } from '@ant-design/icons'
 
-import { Link } from 'react-router-dom'
+const { Header, Content, Footer, Sider } = Layout
 
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
-
-function Dashboard(props) {
-  const { window } = props;
-  const classes = useStyles();
-  const theme = useTheme();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  //const handleClick = e => e.preventDefault()
-
-  const drawer = (
-    <div>
-      <div className={classes.toolbar}>Username</div>
-      <Divider />
-      <List>
-        {['Dashboard', 'Timeline', 'Settings', 'Sign In', 'Sign Out'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <Link to={`/${text.replace(/ +/g, "").toLowerCase()}`}>
-                <ListItemText primary={text} />
-              </Link>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Revolutional App
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <nav className={classes.drawer} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Hidden smUp implementation="css">
-          <Drawer
-            container={container}
-            variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-      </nav>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Typography paragraph variant='h4'>
-            Welcome to dashboard
-        </Typography>
-      </main>
-    </div>
-  );
+function Dashboard() {
+    return (
+        <Layout>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                style={{
+                    height: '100vh'
+                }}
+            >
+                <div className="logo">
+                    <Typography.Text strong className="logo-client-name">
+                        Client's Name
+                    </Typography.Text>
+                </div>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1" icon={<DashboardOutlined />}>
+                        Overview
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<HistoryOutlined />}>
+                        Activities
+                    </Menu.Item>
+                    <Menu.Item key="3" icon={<SettingOutlined />}>
+                        Settings
+                    </Menu.Item>
+                    <Menu.Item key="4" icon={<LogoutOutlined />}>
+                        Sign Out
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout>
+                <Header className="site-layout-sub-header-background" style={{ padding: 0 }}>
+                    <Typography.Text strong style={{color: '#fff', paddingLeft: 19}}>
+                        Revolutional App
+                    </Typography.Text>
+                </Header>
+                <Content style={{ margin: '24px 16px 0' }}>
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                        <Row>
+                            <Col span={12}>Stock</Col>
+                            <Col span={12}>Profit</Col>
+                        </Row>
+                        <Row>
+                            <Col span={12}>Referal Bonus</Col>
+                            <Col span={12}>Loss</Col>
+                        </Row>
+                        <Row>
+                            <Col span={24}>
+                                <Button type="primary" block>Buy Stock</Button>
+                            </Col>
+                        </Row>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Revolutional App ©{new Date().getFullYear()}</Footer>
+            </Layout>
+        </Layout>
+    )
 }
 
-/*Dashboard.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   
-  window: PropTypes.func,
-};*/
-
-export default Dashboard;
+export default Dashboard
